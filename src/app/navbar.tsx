f6fa0e34b23633ad32f5f61cb54f6e9cc0bd1fc6
Bisheshwar Das <officialbisheshwar@gmail.com>
2024-08-03 21:50:26 +0545

    bleh bleh bleh testing

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -5,6 +5,7 @@ export default function Navbar(){
         <div className="bg-indigo-200 drop-shadow-xl text-xl text-black p-4 flex justify-center space-x-5">
             <Link href="/">HOME</Link>
             <Link href="/about">ABOUT</Link>
+            <Link href="/about">BDAS here</Link>
         </div>
     )
 }
